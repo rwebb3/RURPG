b93543d38d226412a0fad996074f5111e79af3ee
spiderobert <spiderobert@gmail.com>
2014-11-15 18:09:39 -0500

    First commit

---
+++ Assets/NGUISwitchPanel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("NGUI/Interaction/Switch Menu AB")]
+public class NGUISwitchPanel : MonoBehaviour {
+	
+	public GameObject currentPanelObject;
+	public GameObject nextPanelObject;
+	
+	void OnClick() {
+		NGUITools.SetActive(nextPanelObject, true);
+		NGUITools.SetActive(currentPanelObject, false);
+	}
+	
+}
---
+++ Assets/SplashBehavior.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashBehavior : MonoBehaviour {
+
+	public GameObject currentPanelObject;
+	public GameObject nextPanelObject;
+	float timer = 2.0f;
+	
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timer <= 1){
+			NGUITools.SetActive(nextPanelObject, true);
+			NGUITools.SetActive(currentPanelObject, false);
+			}
+		timer -= Time.deltaTime;
+	}
+}
