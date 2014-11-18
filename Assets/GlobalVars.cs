14eed5572930832010acbe60e7342fd87ae734a3
spiderobert <spiderobert@gmail.com>
2014-11-17 20:52:53 -0500

    cleanup and audio controls
    removed a lot of assets that we didn't need and made disabling of sound
    and music possible.
    
    thought ahead and made global variables for sound be floats so we can
    add a slider later instead of simply "on" or "off"

---
+++ Assets/GlobalVars.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalVars : MonoBehaviour {
+	public static float musicVolume = 1.0f;
+	public static float soundVolume = 1.0f;
+
+
+}
---
+++ Assets/setSoundVolume.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class setSoundVolume : MonoBehaviour {
+
+	void OnClick() {
+		if (GlobalVars.soundVolume == 1.0f){
+			GlobalVars.soundVolume = 0.0f;}
+		else{
+			GlobalVars.soundVolume = 1.0f;}
+	}
+}
