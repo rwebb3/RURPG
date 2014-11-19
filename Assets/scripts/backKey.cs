2db686793fb2ac2c3b0943697ed93695443f7a0a
spiderobert <spiderobert@gmail.com>
2014-11-18 23:25:55 -0500

    back buttons
    changed back button script

---
+++ Assets/scripts/backKey.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class backKey : MonoBehaviour {
+
+	public bool exitProgram;
+	public GameObject currentPanelObject;
+	public GameObject previousPanelObject;
+	
+	void OnClick () {
+		if (exitProgram) {Application.Quit ();}
+		else {
+			NGUITools.SetActive(previousPanelObject, true);
+			NGUITools.SetActive(currentPanelObject, false);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//code makes back buttons on Android devices work
+		if (Application.platform == RuntimePlatform.Android)
+		{
+			if (Input.GetKey(KeyCode.Escape))
+			{
+				if (exitProgram) {Application.Quit();}
+				else {
+					NGUITools.SetActive(previousPanelObject, true);
+					NGUITools.SetActive(currentPanelObject, false);
+				}
+
+			}
+		}
+	}
+}
