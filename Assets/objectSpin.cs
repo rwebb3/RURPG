db56d2fb742db4b096604d7fa88aa87b4c970782
spiderobert <spiderobert@gmail.com>
2015-02-22 18:39:29 -0500

    added a key
    added key to tutorial level.

---
+++ Assets/objectSpin.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class objectSpin : MonoBehaviour { 
+	public float speed;
+
+	private bool playerIsNear = false; 
+
+	void OnTriggerStay2D(Collider2D other){
+		if (LayerMask.LayerToName(other.gameObject.layer).Equals("Player")){
+			playerIsNear = true;
+		}
+	}
+	
+	void OnTriggerExit2D(Collider2D other){
+		if (LayerMask.LayerToName(other.gameObject.layer).Equals("Player")){
+			this.transform.localEulerAngles = new Vector3(0,0,-45);
+			playerIsNear = false;
+		}
+	}
+
+	void OnMouseDown(){
+		if (playerIsNear){
+			Destroy(this.gameObject);
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+		if (playerIsNear){
+			this.transform.RotateAround(this.transform.position, Vector3.up, Time.deltaTime * speed);
+			}
+	}
+}
