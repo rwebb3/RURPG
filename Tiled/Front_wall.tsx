463994652c1f44d08fa76af1c89442b82faaa6db
spiderobert <spiderobert@gmail.com>
2015-02-22 16:53:10 -0500

    added tiled files
    tiled files are under Assets > Tiled

---
+++ Tiled/Front_wall.tsx
@@ -0,0 +1,54 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Front_wall" tilewidth="16" tileheight="64">
+ <image source="Front_wall.png" width="96" height="64"/>
+ <tile id="0">
+  <properties>
+   <property name="unity:layer" value="wall"/>
+   <property name="unity:sortingLayerName" value="wall"/>
+  </properties>
+  <objectgroup draworder="index"/>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="unity:layer" value="wall"/>
+   <property name="unity:sortingLayerName" value="wall"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="0" x="0.125" y="10.625" width="15.7273" height="4"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="unity:layer" value="wall"/>
+   <property name="unity:sortingLayerName" value="wall"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="0" x="0.0830039" y="11.1102" width="15.8478" height="3.89674"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="unity:layer" value="wall"/>
+   <property name="unity:sortingLayerName" value="wall"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="0" x="-0.0909091" y="10.1932" width="15.9091" height="3.90909"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="unity:layer" value="wall"/>
+   <property name="unity:sortingLayerName" value="wall"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="0" x="0.25" y="10.875" width="15.625" height="3.75"/>
+   <object id="0" x="9.125" y="13.5" height="0.125"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="unity:layer" value="wall"/>
+   <property name="unity:sortingLayerName" value="wall"/>
+  </properties>
+ </tile>
+</tileset>
---
+++ Tiled/Top_Wall.tsx
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Top_Wall" tilewidth="16" tileheight="16">
+ <image source="Top_Wall.png" width="32" height="64"/>
+ <tile id="0">
+  <properties>
+   <property name="unity:layer" value="wall"/>
+   <property name="unity:sortingLayerName" value="wall"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="0" x="0.0909091" y="0" width="15.7273" height="16.0909"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="unity:layer" value="wall"/>
+   <property name="unity:sortingLayerName" value="wall"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="0" x="0.0909091" y="0.0909091" width="15.6364" height="15.8182"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="unity:layer" value="wall"/>
+   <property name="unity:sortingLayerName" value="wall"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="0" x="0.0909091" y="0.0909091" width="15.8182" height="15.8182"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="unity:layer" value="wall"/>
+   <property name="unity:sortingLayerName" value="wall"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="0" x="8.32667e-17" y="-0.363636" width="15.9091" height="16.3636"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="unity:layer" value="wall"/>
+   <property name="unity:sortingLayerName" value="wall"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="0" x="0.0909091" y="-0.0909091" width="15.7273" height="16.0909"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="unity:layer" value="wall"/>
+   <property name="unity:sortingLayerName" value="wall"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="0" x="0.0909091" y="0" width="15.8182" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="unity:layer" value="wall"/>
+   <property name="unity:sortingLayerName" value="wall"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="0" x="0.0909091" y="0.181818" width="15.7273" height="15.9091"/>
+  </objectgroup>
+ </tile>
+</tileset>
