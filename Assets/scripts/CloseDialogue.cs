216441b619db1483d80275b953d1b1071ff66999
sberlin92 <berlin.sean@gmail.com>
2015-02-22 19:58:36 -0500

    Dialogue Text
    Dialogue Text at start of the level

---
+++ Assets/scripts/CloseDialogue.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloseDialogue : MonoBehaviour {
+
+	public GameObject menu;
+	private bool isShowing = true;
+
+	// Use this for initialization
+	public void Okay () {
+		isShowing = false;
+		menu.SetActive (isShowing);
+	}
+
+}
