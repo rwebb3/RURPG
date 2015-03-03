4277ccee7a9e408a008ef738c555bd4d4302ad37
spiderobert <spiderobert@gmail.com>
2015-03-03 00:32:02 -0500

    added short dialogue options to NPC

---
+++ Assets/NewPause.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewPause : MonoBehaviour {
+
+	public GameObject pausePanel;
+
+	// Update is called once per tick
+	void FixedUpdate() {
+		if (Input.GetKey(KeyCode.Escape)){
+			pausePanel.SetActive(true);
+		}			
+	}
+}
---
+++ Assets/scripts/ShowOtherPanel.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowOtherPanel : MonoBehaviour {
+
+	public GameObject thePanelToHide;
+	public GameObject thePanelToShow;
+	
+	public void Start(){
+		//thePanelToClose = this.transform.parent.gameObject;
+	}
+	
+	//Button should close Help Text
+	public void UseButton() {
+		thePanelToHide.SetActive(false);
+		thePanelToShow.SetActive(true);
+	}
+}
