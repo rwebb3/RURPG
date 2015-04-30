684019dfe500af805cc226d9d66d4ef97b33763b
spiderobert <spiderobert@gmail.com>
2015-04-30 00:51:59 -0400

    added message about skipping to end

--- Assets/scripts/GlobalVars.cs
+++ Assets/scripts/GlobalVars.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Collections;
 
 public class GlobalVars : MonoBehaviour {
-	public static float musicVolume = 1.0f;
+	public static float musicVolume = 0.3f;
 	public static float soundVolume = 1.0f;
 
 	public static Inventory currentInventory = new Inventory();
