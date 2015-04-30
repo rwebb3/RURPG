5556b0b26f23c81e3cd45081f2f03201820afcb9
Robert Webb <rwebb3@radford.edu>
2015-04-30 11:09:05 -0400

    updated APK

---
+++ Assets/PauseAfterSeconds.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseAfterSeconds : MonoBehaviour {
+
+	public float waitToPause;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (waitToPause > 0){
+			waitToPause -= Time.deltaTime;
+		}
+		if (waitToPause <= 0){
+			Time.timeScale = 0;
+		}
+			
+	}
+}
